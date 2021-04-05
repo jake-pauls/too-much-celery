6faf4f66ad9b5183326d7c4ac1222222c6cc8db3
jacob-pauls <jakepauls99@gmail.com>
2021-04-04 21:39:38 -0700

    Basic flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def ping():
+    return { "ping": "pong" }
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
